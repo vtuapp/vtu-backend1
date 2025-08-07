4a362cabe2893cf0775de6e12587aab5a00f5f38
Your Name <your@email.com>
2025-08-07 18:36:11 +0100

    Update backend with latest changes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 // routes/users.js
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Register Route
 router.post("/register", async (req, res) => {
